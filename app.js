f0d01a389a957598bd45cbaee55c02af1de89877
dukeossuna <andregsistemas@outlook.com>
2023-10-31 14:33:11 -0300

    Arquivos - Condicionais e concatenação

---
+++ app.js
@@ -0,0 +1,11 @@
+alert('Boas vindas ao jogodo número secreto!');
+
+let numeroSecreto = 5;
+
+let chute = prompt('Escolha um número entre 1 e 30');
+
+if(numeroSecreto == chute) {
+    console.log('Isso ai vocẽ descobriu o número sercreto');
+}else{
+    console.log('Que pena você errou o número!');
+}
