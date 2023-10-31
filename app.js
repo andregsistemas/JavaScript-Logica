6cb352907869f78d3a39a8c9115730787967e41f
dukeossuna <andregsistemas@outlook.com>
2023-10-31 17:00:38 -0300

    Arquivos - Loops e tentativas

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 alert('Boas vindas ao jogodo número secreto!');
-
-let numeroSecreto = 5;
-
+let numeroSecreto = 4;
 let chute = prompt('Escolha um número entre 1 e 30');
 
+//se chute for igual ao número secreto
 if(numeroSecreto == chute) {
-    console.log('Isso ai vocẽ descobriu o número sercreto');
-}else{
-    console.log('Que pena você errou o número!');
+    alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto}`); //Template Strings
+}else {
+    alert('Que pena você errou o número!');
 }
