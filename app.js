dc9eaf066d94a78f0f28034474cb94e9edb48457
dukeossuna <andregsistemas@outlook.com>
2023-10-31 18:00:19 -0300

    Boas práticas de programação

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 alert('Boas vindas ao jogodo número secreto!');
-let numeroSecreto = 4;
-let chute = prompt('Escolha um número entre 1 e 30');
+let numeroSecreto = 5;
+let chute = null;
+let tentativas = 1;
 
-//se chute for igual ao número secreto
-if(numeroSecreto == chute) {
-    alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto}`); //Template Strings
-}else {
-    alert('Que pena você errou o número!');
+// Enquanto chute não for iguaç ao numeroSecreto
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 30');
+    //se chute for igual ao número secreto
+    if(chute == numeroSecreto) {
+        alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto} com ${tentativas} tentativas`); //Template Strings
+    }else {
+        if(chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}`);
+        }else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas ++
+    }
 }
