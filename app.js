96949f8f267a18b89dfa32099f2e54b3092623e5
dukeossuna <andregsistemas@outlook.com>
2023-11-01 10:52:27 -0300

    Arquivos - Desafios

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 alert('Boas vindas ao jogodo número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 let chute = null;
 let tentativas = 1;
 
 // Enquanto chute não for iguaç ao numeroSecreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 30');
+    chute = prompt('Escolha um número entre 1 e 100');
     //se chute for igual ao número secreto
     if(chute == numeroSecreto) {
-        alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto} com ${tentativas} tentativas`); //Template Strings
+        break;
+        
     }else {
         if(chute > numeroSecreto){
             alert(`O número secreto é menor que ${chute}`);
@@ -19,3 +20,14 @@ while (chute != numeroSecreto) {
         tentativas ++
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Vocẽ descobriu o número sercreto que é o ${numeroSecreto} usando ${tentativas} ${palavraTentativa}`); //Template Strings
+
+/*
+if(tentativas > 1){
+    alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto} usando ${tentativas} tentativas!`); //Template Strings
+}else{
+    alert(`Isso ai vocẽ descobriu o número sercreto que é o ${numeroSecreto} com apenas ${tentativas} tentativa!`); //Template Strings
+}
+*/
